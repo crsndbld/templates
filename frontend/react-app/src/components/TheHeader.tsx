42833b3ad06f3d81c84065c6a7d0ebbbaf6e003c
crsndbld <90888205+crsndbld@users.noreply.github.com>
2022-03-11 23:53:42 +0900

    template created

---
+++ frontend/react-app/src/components/TheHeader.tsx
@@ -0,0 +1,13 @@
+import React, { VFC } from "react";
+
+export interface TheHeader {}
+type Props = {};
+const TheHeader: VFC<Props> = () => {
+  return (
+    <>
+      <p>TheHeader</p>
+    </>
+  );
+};
+
+export default TheHeader;
